6c4d8d5844119929dc0790d5893a5c4ac4de8c4f
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-06-22 06:27:40 -0400

    Added solutions

--- 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0319/Solution.java
@@ -10,9 +10,11 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         String name = reader.readLine();
         String num1 = reader.readLine();
         String num2 = reader.readLine();
+
         System.out.println(name + " получает " + num1 + " через " + num2 + " лет.");
     }
 }
