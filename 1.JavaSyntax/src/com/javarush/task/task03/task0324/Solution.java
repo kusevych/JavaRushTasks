06ca8f51055040290a60f8b8366a2370fdf1c07f
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-07-09 10:25:10 -0400

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task03/task0324/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task03.task0324;
+
+/* 
+Меркантильные намерения
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+            i++;
+        }
+    }
+}
