bdc4950af77eb2c8a6591b3581d56a9d5fbdc132
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-08-02 13:49:38 -0400

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task04/task0434/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task04.task0434;
+
+/* 
+Таблица умножения Ӏ Java Syntax: 4 уровень, 10 лекция
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 10) {
+            int j = 1;
+            while (j <= 10) {
+                System.out.print(j * i + " ");
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
