09fbb57bab000613bb0b8e270cad1eec5170f473
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-09-10 15:54:55 -0400

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0606/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.task06.task0606;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Чётные и нечётные циферки
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(reader.readLine());
+        int rem;
+        while (num > 0) {
+            rem = num % 10;
+            if (rem % 2 == 0)
+                even++;
+            else
+                odd++;
+            num /= 10;
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
+    }
+}
