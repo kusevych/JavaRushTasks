bad6cb4cace0c8c3826150673eab02dce542c2e3
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-09-10 15:35:57 -0400

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task06/task0603/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task06.task0603;
+
+/* 
+По 50 000 объектов Cat и Dog
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i < 50000) {
+            new Cat();
+            new Dog();
+            i++;
+        }
+    }
+}
+
+class Cat {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("A Cat was destroyed");
+    }
+}
+
+class Dog {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("A Dog was destroyed");
+    }
+}
