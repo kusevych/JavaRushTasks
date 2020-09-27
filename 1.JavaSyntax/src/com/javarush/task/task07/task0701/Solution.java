785afbb790e641b48688a6d7ca92e95af5944b49
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-09-27 15:46:38 -0400

    Added solutions

--- 1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task07/task0701/Solution.java
@@ -27,7 +27,7 @@ public class Solution {
     public static int max(int[] array) {
         int max = array[0];
         for (int i = 1; i < array.length; i++) {
-            if(array[i] > max)
+            if (array[i] > max)
                 max = array[i];
         }
         return max;
