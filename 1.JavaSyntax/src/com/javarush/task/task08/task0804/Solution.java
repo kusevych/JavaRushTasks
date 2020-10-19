d7a226552ad25837e663714c85acd2f8a13f96ed
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-10-19 15:28:48 -0400

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0804/Solution.java
@@ -0,0 +1,36 @@
+package com.javarush.task.task08.task0804;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/* 
+Вывести на экран список ключей
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+        map.put("Sim", "Sim");
+        map.put("Tom", "Tom");
+        map.put("Arbus", "Arbus");
+        map.put("Baby", "Baby");
+        map.put("Cat", "Cat");
+        map.put("Dog", "Dog");
+        map.put("Eat", "Eat");
+        map.put("Food", "Food");
+        map.put("Gevey", "Gevey");
+        map.put("Hugs", "Hugs");
+
+        printKeys(map);
+    }
+
+    public static void printKeys(Map<String, String> map) {
+        Iterator <Map.Entry<String, String>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> pair = iterator.next();
+            String key = pair.getKey();
+            System.out.println(key);
+        }
+    }
+}
