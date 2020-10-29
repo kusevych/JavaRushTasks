8f6b1ab25f0d53cd21ccd2f6202ce4ca537476dd
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-10-29 10:10:58 -0400

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0809/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task08.task0809;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Время для 10 тысяч вставок
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(getInsertTimeInMs(new ArrayList()));
+        System.out.println(getInsertTimeInMs(new LinkedList()));
+    }
+
+    public static long getInsertTimeInMs(List list) {
+        Date starTime = new Date();
+
+        insert10000(list);
+        Date newTime = new Date();
+        long ms = newTime.getTime() - starTime.getTime();
+        return ms;
+    }
+
+    public static void insert10000(List list) {
+        for (int i = 0; i < 10000; i++) {
+            list.add(0, new Object());
+        }
+    }
+}
