43b2286237e25c96b95347f042ec1bf07313019b
Anzhelika Kusevych <lika.kusevich@gmail.com>
2020-12-09 10:42:12 -0500

    Added solutions

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0138/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task01.task0138;
+
+/* 
+Любимое стихотворение
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Мое любимое стихотворение:");
+        System.out.println("С любимыми не расставайтесь");
+        System.out.println("Всем сердцем проростайте в них");
+        System.out.println("И каждый раз на век прощайтесь");
+        System.out.println("Когда уходите на миг");
+    }
+}
